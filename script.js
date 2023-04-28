3e4cd04709011b0331a52fda00ec9cf5fac82a6c
Rahul Dev <rahuldev7583@gmail.com>
2023-04-29 01:06:24 +0530

    update

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-var audio = document.createElement('audio');
-audio.setAttribute('src', 'sound.mp3');
-audio.loop=true;
-audio.play();
+const audio = new Audio("sound.mp3");
+const button = document.getElementById("play-button");
+const icon = document.getElementById("icon");
+
+window.addEventListener("load", () => {
+  button.addEventListener("click", () => {
+    if (icon.src.substring(icon.src.lastIndexOf("/") + 1) === "soundoff.png") {
+      icon.src = "sound.png";
+      audio.loop = true;
+      audio.play();
+    } else {
+      icon.src = "soundoff.png";
+      audio.pause();
+      audio.currentTime = 0;
+    }
+  });
+});
